c917cdf60ae75838f7263e8f8fbc336538d7e533
Praveen Chamod <praveen.c@fidenz.com>
2024-02-21 09:51:58 +0530

    Add new ui texts.

---
+++ src/consts/uiTexts.ts
@@ -0,0 +1,5 @@
+export namespace UiTexts{
+    export const introduction : string = "An energetic and experienced software engineer with 1+ years of industry hands on experience. I have specialized my career as a full-stack web developer while learning latest industry standards and trends. I am a person who takes responsibility of my work very well. I am a natural problem solver, who has proven myself by completing given projects on time.";
+
+}
+
