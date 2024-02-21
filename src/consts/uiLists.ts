f6f29dcbaaecc4f5167b2c897a884ae6e7674305
Praveen Chamod <praveen.c@fidenz.com>
2024-02-21 09:51:40 +0530

    Add new ui lists.

---
+++ src/consts/uiLists.ts
@@ -0,0 +1,5 @@
+export namespace uiLists {
+    export const frontendSkills: string[] = ['JavaScript', 'HTML/CSS', 'TailwindCSS', 'TypeScript', 'React', 'Redux', 'MaterialUI', 'Bootstrap'];
+    export const backendSkills: string[] = ['.NET', 'EF Core', 'Docker', 'CI/CD', 'Node', 'Express.js', 'MySQL', 'PostgreSQL'];
+    export const otherSkills: string[] = ['Teamwork', 'Time Management', 'Communication', 'Adaptability'];
+}
