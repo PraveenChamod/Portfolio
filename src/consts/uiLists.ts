272403082da23358c9d8d486a255cc086d7c108e
Praveen Chamod <praveen.c@fidenz.com>
2024-07-19 13:27:29 +0530

    Add ui changes.

--- src/consts/uiLists.ts
+++ src/consts/uiLists.ts
@@ -1,5 +1,9 @@
 export namespace uiLists {
-    export const frontendSkills: string[] = ['JavaScript', 'HTML/CSS', 'TailwindCSS', 'TypeScript', 'React', 'Redux', 'MaterialUI', 'Bootstrap'];
-    export const backendSkills: string[] = ['.NET', 'EF Core', 'Docker', 'CI/CD', 'Node', 'Express.js', 'MySQL', 'PostgreSQL'];
-    export const otherSkills: string[] = ['Teamwork', 'Time Management', 'Communication', 'Adaptability'];
+    export const languageExposure: string[] = ['C#', 'Typescript', 'Javascript', 'Dart', 'Jquery', 'Java'];
+    export const frameworks: string[] = ['.NET Framework', '.NET Core', 'Entity Framework', 'Springboot'];
+    export const frontend: string[] = ['HTML5', 'TailwindCSS', 'React', 'JSON', 'MaterialUI', 'Bootstrap', 'Redux'];
+    export const dbManagement: string[] = ['MS SQL Server', 'pgAdmin4'];
+    export const toolsAndPractices: string[] = [ 'Git', 'JIRA', 'Bitbucket', 'Docker', 'Source Tree', 'Confluence', 'Kafka', 'CI/CD'];
+    export const softwareDesign: string[] = ['Object-Oriented Programming', 'Clean Architecture', 'MVC Architecture'];
+    export const otherSkills: string[] = ['Problem Solving', 'Teamwork', 'Time Management', 'Communication', 'Adaptability'];
 }
--- src/features/about/index.tsx
+++ src/features/about/index.tsx
@@ -1,41 +1,79 @@
-import React from 'react'
-import { UiTexts } from '../../consts/uiTexts';
-import TextButton from '../../common/components/buttons/TextButton';
-import { uiLists } from '../../consts/uiLists';
+import React from "react";
+import { UiTexts } from "../../consts/uiTexts";
+import TextButton from "../../common/components/buttons/TextButton";
+import { uiLists } from "../../consts/uiLists";
 
 function About() {
   return (
-    <div className='flex flex-col justify-center items-left w-full p-8 h-full'>
-      <div className='mb-6'>
+    <div className="flex flex-col justify-center items-left w-full p-8 h-full">
+      <div className="mb-6">
         <div>
-          <p className='text-[30px] font-bold'>Intro</p>
+          <p className="text-[30px] font-bold">Intro</p>
         </div>
         <div>
-          <p className='text-xl text-justify'>
-            {UiTexts.introduction}
-          </p>
+          <p className="text-xl text-justify">{UiTexts.introduction}</p>
         </div>
       </div>
-      <div className='mb-6'>
+      <div className="mb-6 flex flex-col gap-6">
         <div>
-          <p className='text-[30px] font-bold'>Technical Skills</p>
+          <p className="text-[30px] font-bold">Technical Skills</p>
         </div>
-        <div className='mt-3'>
+        <div className="mt-3 flex flex-row gap-4">
           <div>
-            <p className='text-xl font-semibold'>Frontend</p>
+            <p className="text-xl font-semibold">Language Exposure:</p>
           </div>
-          <div className='flex flex-wrap gap-3 my-4'>
-            {uiLists.frontendSkills.map((skill, index) => (
+          <div className="flex flex-wrap gap-3 ">
+            {uiLists.languageExposure.map((skill, index) => (
               <TextButton key={index} description={skill} />
             ))}
           </div>
         </div>
-        <div>
+        <div className="mt-3 flex flex-row gap-4">
           <div>
-            <p className='text-xl font-semibold'>Backend</p>
+            <p className="text-xl font-semibold">Framework And Technologies:</p>
           </div>
-          <div className='flex flex-wrap gap-3 my-4'>
-            {uiLists.backendSkills.map((skill, index) => (
+          <div className="flex flex-wrap gap-3">
+            {uiLists.frameworks.map((skill, index) => (
+              <TextButton key={index} description={skill} />
+            ))}
+          </div>
+        </div>
+        <div className="mt-3 flex flex-row gap-4">
+          <div>
+            <p className="text-xl font-semibold">Frontend Technologies:</p>
+          </div>
+          <div className="flex flex-wrap gap-3">
+            {uiLists.frontend.map((skill, index) => (
+              <TextButton key={index} description={skill} />
+            ))}
+          </div>
+        </div>
+        <div className="mt-3 flex flex-row gap-4">
+          <div>
+            <p className="text-xl font-semibold">Database Management:</p>
+          </div>
+          <div className="flex flex-wrap gap-3">
+            {uiLists.dbManagement.map((skill, index) => (
+              <TextButton key={index} description={skill} />
+            ))}
+          </div>
+        </div>
+        <div className="mt-3 flex flex-row gap-4">
+          <div>
+            <p className="text-xl font-semibold">Dev Tools And Practice:</p>
+          </div>
+          <div className="flex flex-wrap gap-3">
+            {uiLists.toolsAndPractices.map((skill, index) => (
+              <TextButton key={index} description={skill} />
+            ))}
+          </div>
+        </div>
+        <div className="mt-3 flex flex-row gap-4">
+          <div>
+            <p className="text-xl font-semibold">Software Design:</p>
+          </div>
+          <div className="flex flex-wrap gap-3">
+            {uiLists.softwareDesign.map((skill, index) => (
               <TextButton key={index} description={skill} />
             ))}
           </div>
@@ -43,16 +81,21 @@ function About() {
       </div>
       <div>
         <div>
-          <p className='text-[30px] font-semibold'>Other Skills</p>
+          <p className="text-[30px] font-semibold">Other Skills</p>
         </div>
-        <div className='flex flex-wrap gap-3 my-4'>
-          {uiLists.otherSkills.map((skill, index) => (
-            <TextButton key={index} description={skill} />
-          ))}
+        <div className="mt-3 flex flex-row gap-4">
+          <div>
+            <p className="text-xl font-semibold">Soft Skills:</p>
+          </div>
+          <div className="flex flex-wrap gap-3">
+            {uiLists.otherSkills.map((skill, index) => (
+              <TextButton key={index} description={skill} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default About;
