f822a44f8b2568ad853d85538615aeacc98ecd2e
Praveen Chamod <praveen.c@fidenz.com>
2024-10-10 16:37:01 +0530

    Modify text button.

--- src/common/components/buttons/TextButton.tsx
+++ src/common/components/buttons/TextButton.tsx
@@ -4,7 +4,7 @@ import { CommonTypes } from '../../../types/common'
 const TextButton = ({ description, onClick }: CommonTypes.TextButtonProps) => {
     return (
         <div>
-            <button onClick={onClick} className="bg-[#e55a66] text-lg text-white transition duration-300 hover:bg-[#EBCB60] hover:text-black px-4 py-1 rounded-full h-auto">{description}</button>
+            <button onClick={onClick} className="bg-[#26bbff] text-lg text-[#101014] transition duration-300 hover:bg-[#61cdff] px-4 py-1 rounded-full h-auto">{description}</button>
         </div>
     )
 }
