0b6db19b1153b484a77cca660350462fb1c2ec99
Praveen Chamod <praveen.c@fidenz.com>
2024-11-25 21:38:55 +0530

    Add color adjustments.

--- src/features/about/index.tsx
+++ src/features/about/index.tsx
@@ -20,7 +20,7 @@ function About() {
           {experienceList.map((experience, index) => (
             <div
               key={index}
-              className="flex flex-col lg:flex-row items-center bg-[#F5F5F7] shadow-lg p-6 rounded-tl-[30px] rounded-bl-[30px] rounded-br-[30px] px-10"
+              className="flex flex-col lg:flex-row items-center bg-gradient-to-tr from-[#F5F5F7] to-violet-500 shadow-lg p-6 rounded-tl-[30px] rounded-bl-[30px] rounded-br-[30px] px-10"
             >
               <a
                 href={experience.link}
@@ -33,7 +33,7 @@ function About() {
                   className="w-60 h-20 object-contain mb-4 lg:mb-0 lg:pr-6"
                 />
               </a>
-              <div className="w-full border-t-2 lg:border-t-0 lg:border-l-2 border-gray-300 pl-0 pt-6 lg:pt-0 lg:pl-6">
+              <div className="w-full border-t-2 lg:border-t-0 lg:border-l-2 border-gray-500 pl-0 pt-6 lg:pt-0 lg:pl-6">
                 <p className="text-[30px] sm:text-[40px] md:text-[40px] lg:text-[40px] text-black">
                   {experience.company}
                 </p>
@@ -43,7 +43,7 @@ function About() {
                 </p>
                 <div className="mt-4 flex flex-wrap gap-2">
                   {experience.skills.map((skill, idx) => (
-                    <div className="text-lg bg-gradient-to-tr from-[#03346E] to-purple-500 border rounded-tl-[30px] rounded-bl-[30px] rounded-br-[30px]">
+                    <div className="text-lg bg-gradient-to-tr from-[#03346E] to-purple-500 rounded-tl-[30px] rounded-bl-[30px] rounded-br-[30px]">
                       <p className="p-2">{skill}</p>
                     </div>
                   ))}
@@ -59,7 +59,7 @@ function About() {
           Acedemic Excellence
         </p>
         <div className="grid grid-cols-1 md:grid-cols-1 gap-12">
-          <div className="flex flex-col lg:flex-row items-center bg-[#F5F5F7] shadow-lg p-6 rounded-tl-[30px] rounded-bl-[30px] rounded-br-[30px] px-10">
+          <div className="flex flex-col lg:flex-row items-center bg-gradient-to-tr from-[#F5F5F7] to-violet-300 shadow-lg p-6 rounded-tl-[30px] rounded-bl-[30px] rounded-br-[30px] px-10">
             <div className="w-full pl-0 lg:pl-6">
               <p className="text-[30px] sm:text-[40px] md:text-[40px] lg:text-[40px] text-black">
                 Faculty of Science
@@ -69,7 +69,7 @@ function About() {
               </p>
               <p className="text-lg text-black">Feb 2020 - Feb 2024</p>
             </div>
-            <div className="w-full border-t-2 lg:border-t-0 lg:border-l-2 border-gray-300 pl-0 mt-4 lg:pt-0 lg:pl-6">
+            <div className="w-full border-t-2 lg:border-t-0 lg:border-l-2 border-gray-500 pl-0 mt-4 lg:pt-0 lg:pl-6">
               <p className="text-[30px] sm:text-[40px] md:text-[40px] lg:text-[40px] text-black">
                 Bachelor Of Computer Science (BCS)
               </p>
@@ -79,8 +79,8 @@ function About() {
               <p className="text-lg text-black">GPA: 3.21 / 4.00</p>
             </div>
           </div>
-          <div className="flex flex-col lg:flex-row items-center bg-[#F5F5F7] shadow-lg p-6 rounded-tl-[30px] rounded-bl-[30px] rounded-br-[30px] px-10">
-            <div className="w-full pl-0 lg:pl-6 border-b-2 lg:border-b-0 lg:border-r-2 border-gray-300 pb-4 lg:pb-0" >
+          <div className="flex flex-col lg:flex-row items-center bg-gradient-to-tr from-[#F5F5F7] to-violet-300 shadow-lg p-6 rounded-tl-[30px] rounded-bl-[30px] rounded-br-[30px] px-10">
+            <div className="w-full pl-0 lg:pl-6 border-b-2 lg:border-b-0 lg:border-r-2 border-gray-500 pb-4 lg:pb-0" >
               <p className="text-[30px] sm:text-[40px] md:text-[40px] lg:text-[40px] text-black">
                 Bandaranayake Central College
               </p>
