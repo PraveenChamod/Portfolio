a4ee35d95c091642cea5ecc8ac4db4c18c09fda1
Praveen Chamod <praveen.c@fidenz.com>
2024-11-27 21:15:12 +0530

    Add footer.

--- src/features/page/index.tsx
+++ src/features/page/index.tsx
@@ -18,6 +18,15 @@ function page() {
       <section id="contact">
         <Contact />
       </section>
+      <footer className="text-[#75757a] mb-12">
+        <div className="flex items-center justify-center">
+          <span className="text-lg">&copy;</span>
+          <span className="pl-2">2024 Praveen Chamod</span>
+        </div>
+        <div className="flex items-center justify-center">
+          <span className="pl-1">All rights reserved.</span>
+        </div>
+      </footer>
     </div>
   );
 }
